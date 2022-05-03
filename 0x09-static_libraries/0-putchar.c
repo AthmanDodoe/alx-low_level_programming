f9eedc96fed0dc5c3fd0a1b7dd7cea7c71ce88d0
AthmanDodoe <jilloathman943@gmail.com>
2022-05-03 18:22:12 +0300

    0x09-static_libraries

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*main - function prints text as output
+*
+*Return:return 0
+*/
+int main(void)
+{
+	char arr[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(arr[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
